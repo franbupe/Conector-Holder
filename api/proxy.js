b07c1c6a277182b9719f5471ebe6f6ebbcfef845
franbupe <franbupe@gmail.com>
2024-11-12 18:20:13 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,3 +1,4 @@
+
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
@@ -9,23 +10,26 @@ export default async function handler(req, res) {
             method: req.method,
             headers: {
                 accept: 'application/json',
-                key: '41c0fff04435e0638a6406d64376d702', // Coloca aquí tu API Key de Holded
+                key: '41c0fff04435e0638a6406d64376d702' // Reemplaza con tu API Key de Holded
             }
         });
 
-        // Extrae los datos de la respuesta
+        if (!response.ok) {
+            throw new Error(`Error en la respuesta de Holded: ${response.status} ${response.statusText}`);
+        }
+
         const data = await response.json();
 
-        // Configurar los encabezados para permitir CORS
+        // Configuración de CORS
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, key');
 
-        // Responder con los datos recibidos de la API
+        // Responder con los datos de la API
         res.status(response.status).json(data);
     } catch (error) {
         console.error('Error al obtener datos de la API de Holded:', error);
-        res.status(500).json({ error: 'Error al obtener datos de la API de Holded' });
+        res.status(500).json({ error: 'Error al obtener datos de la API de Holded', details: error.message });
     }
 }
 
