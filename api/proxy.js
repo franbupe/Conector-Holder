281991990b5513bd04eb647488a3734b4405b122
franbupe <franbupe@gmail.com>
2024-11-12 18:08:17 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -4,16 +4,28 @@ export default async function handler(req, res) {
     const { path } = req.query;
     const url = `https://api.holded.com/api/${path}`;
 
-    const response = await fetch(url, {
-        method: req.method,
-        headers: {
-            ...req.headers,
-            key: 'YOUR_API_KEY', // Coloca aquí tu API Key de Holded
-            host: 'api.holded.com',
-            'Access-Control-Allow-Origin': '*'
-        }
-    });
+    try {
+        const response = await fetch(url, {
+            method: req.method,
+            headers: {
+                accept: 'application/json',
+                key: '41c0fff04435e0638a6406d64376d702', // Coloca aquí tu API Key de Holded
+            }
+        });
 
-    const data = await response.json();
-    res.status(response.status).json(data);
+        // Extrae los datos de la respuesta
+        const data = await response.json();
+
+        // Configurar los encabezados para permitir CORS
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, key');
+
+        // Responder con los datos recibidos de la API
+        res.status(response.status).json(data);
+    } catch (error) {
+        console.error('Error al obtener datos de la API de Holded:', error);
+        res.status(500).json({ error: 'Error al obtener datos de la API de Holded' });
+    }
 }
+
